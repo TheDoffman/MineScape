23416435535f5ad9357eb9a853036a1b369051af
Dan Hoffman <thedoffman@gmail.com>
2023-08-25 01:39:01 -0400

    Added CraftingSkill

---
+++ src/main/java/org/hoffmantv/minescape/skills/CraftingSkill.java
@@ -0,0 +1,43 @@
+package org.hoffmantv.minescape.skills;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.hoffmantv.minescape.managers.SkillManager;
+
+public class CraftingSkill implements Listener {
+
+    private final SkillManager skillManager;
+
+    public CraftingSkill(SkillManager skillManager){
+        this.skillManager = skillManager;
+    }
+
+    @EventHandler
+    public void onItemCraft(CraftItemEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        ItemStack craftedItem = event.getCurrentItem();
+        if (craftedItem == null) {
+            return;
+        }
+
+        // Calculate XP reward based on the crafted item
+        int xpAmount = calculateXpReward();
+
+        // Add the XP reward to the player's CRAFTING skill using the SkillManager
+        skillManager.addXP(player, SkillManager.Skill.CRAFTING, xpAmount);
+
+        // Notify the player about the XP gained
+        player.sendActionBar(ChatColor.GOLD + "Cooking +" + xpAmount);
+        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+    }
+
+    private int calculateXpReward() {
+        return 15; // Base XP for all items
+    }
+
+}
